31e7d70892204ccc5f1776dc6baf0a8b5f209973
lost-alchemistt <Chetan.v.kalsariya@gmail.com>
2025-09-24 10:08:10 +0530

    Added !!

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import LandingPage from "@/components/Landing/page"
+import InvitationScroller from "@/components/Sections/InvitationScroller";
 export default function Home() {
   return (
     <>
           <LandingPage/>
+          <InvitationScroller/>
     </>
   );
 }
