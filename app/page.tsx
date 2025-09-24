34932c28839370e0c74e8e69d83912d76e4a7214
lost-alchemistt <Chetan.v.kalsariya@gmail.com>
2025-09-24 10:11:47 +0530

    Final scrollar added !!

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import LandingPage from "@/components/Landing/page"
-import InvitationScroller from "@/components/Sections/InvitationScroller";
+// import InvitationScroller from "@/components/Sections/InvitationScroller";
 export default function Home() {
   return (
     <>
           <LandingPage/>
-          <InvitationScroller/>
+          {/* <InvitationScroller/> */}
     </>
   );
 }
